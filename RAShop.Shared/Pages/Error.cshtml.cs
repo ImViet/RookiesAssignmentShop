63583bb03503cae811efb7d454645290f75ac47e
ImViet <dinhquocviet318@gmail.com>
2022-10-14 09:35:06 +0700

    Create new project RAShop.Shared

--- RAShop.Shared/Pages/Error.cshtml.cs
+++ RAShop.Shared/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace RAShop.Pages
+namespace RAShop.Shared.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
