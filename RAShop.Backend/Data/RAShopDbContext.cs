fca8f75aaf7e1b490a09d991dc496d5fae4058a6
ImViet <dinhquocviet318@gmail.com>
2022-10-16 10:54:23 +0700

    Update code

---
+++ RAShop.Backend/Data/RAShopDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RAShop.Backend.Models;
+namespace RAShop.Backend.Data
+{
+    public class RAShopDbContext: DbContext 
+    {
+        private DbSet<Product> Products { get; set; }
+        private DbSet<Category> Categories { get; set; }
+        private DbSet<ProductImage> ProductImages { get; set; }
+        private DbSet<Order> Orders { get; set; }
+        private DbSet<OrderDetail> OrderDetails { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
