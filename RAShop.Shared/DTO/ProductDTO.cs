5191039df357ee2a05ad50a7a0c9bfd2f82e1642
ImViet <dinhquocviet318@gmail.com>
2022-10-17 14:55:16 +0700

    Update project Shared and create 2 class DTO for Product, Category

---
+++ RAShop.Shared/DTO/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RAShop.Shared.DTO
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public string Origin { get; set; }
+        public string Unit { get; set; }
+        public int CategoryName { get; set; }
+        public string MainImg { get; set; }
+    }
+}
