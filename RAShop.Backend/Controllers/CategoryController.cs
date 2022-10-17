74d4a1736ef3e43d9ed90528ed8bfaa5a10a31df
ImViet <dinhquocviet318@gmail.com>
2022-10-17 17:13:47 +0700

    Test: Using AutoMapper to map from model in backend to class dto

--- RAShop.Backend/Controllers/CategoryController.cs
+++ RAShop.Backend/Controllers/CategoryController.cs
@@ -2,9 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using RAShop.Backend.Models;
 using RAShop.Backend.Data;
+using RAShop.Shared.DTO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.ComponentModel;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace RAShop.Backend.Controllers
 {
@@ -13,51 +16,72 @@ namespace RAShop.Backend.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly RAShopDbContext _context;
-        public CategoryController(RAShopDbContext context)
+        private readonly IMapper _mapper;
+        public CategoryController(RAShopDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
+        //Lay tat ca loai san pham
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Category>>> GetAllCategory()
+        public async Task<ActionResult<CategoryDTO>> GetAllCategory()
         {
             var categories = await _context.Categories.ToListAsync();
-            return categories;
+            CategoryDTO cateDto = _mapper.Map<CategoryDTO>(categories);
+            return cateDto;
         }
-        //[HttpGet("{id}")]
-        //public Category GetCategoryById(int id)
-        //{
-        //    var category =  _context.Categories.FirstOrDefault(x => x.CategoryId == id);
-        //    return category;
-        //}
-        //[HttpPost]
-        //public Category CreateCategory(Category newCategory)
-        //{
-        //    _context.Categories.Add(newCategory);   
-        //    _context.SaveChanges();
-        //    return newCategory;
-        //}
-        //[HttpPut("{id}")]
-        //public Category EditCategory(int id, Category newCategory)
-        //{
-        //    var category = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
-        //    if (category != null)
-        //    {
-        //        category = newCategory;
-        //        _context.SaveChanges();
-        //    }
-        //    return category;
-        //}
-        //[HttpDelete("{id}")]
-        //public Category DeleteCategory(int id)
-        //{
-        //    var category = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
-        //    if (category != null)
-        //    {
-        //        _context.Categories.Remove(category);
-        //        _context.SaveChanges();
-        //    }
-        //    return category;
-        //}
+
+        //Lay loai san pham theo id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryDTO>> GetCategoryById(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.CategoryId == id);
+            //return category;
+            CategoryDTO cateDto = _mapper.Map<CategoryDTO>(category);
+            return cateDto;
+        }
+
+        //Tao moi loai san pham
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCate(Category newCate)
+        {
+            var cate = new Category()
+            {
+                CategoryId = newCate.CategoryId,
+                CategoryName = newCate.CategoryName,
+                Description = newCate.Description
+            };
+            await _context.Categories.AddAsync(cate);
+            await _context.SaveChangesAsync();
+            return cate;
+        }
+
+        //Sua mot category
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Category>> EditCategory(int id, Category newCategory)
+        {
+            var category = _context.Categories.Find(id);
+            if (category != null)
+            {
+                category.CategoryName = newCategory.CategoryName;
+                category.Description = newCategory.Description;
+            }
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        //Xoa category
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Category>> DeleteCategory(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (category != null)
+            {
+                 _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+            return category;
+        }
     }
 }
---
+++ RAShop.Backend/Utilities/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using RAShop.Backend.Models;
+using RAShop.Shared.DTO;
+namespace RAShop.Backend.Utilities.AutoMapper
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Category, CategoryDTO>();
+        }
+    }
+}
