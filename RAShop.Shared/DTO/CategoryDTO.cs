cf37c4af9bdff3de140b872947551618f17dfaef
ImViet <dinhquocviet318@gmail.com>
2022-10-18 16:18:04 +0700

    Fix DTO

--- RAShop.Shared/DTO/CategoryDTO.cs
+++ RAShop.Shared/DTO/CategoryDTO.cs
@@ -3,7 +3,7 @@
     public class CategoryDTO
     {
         public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
+        public string CategoryName { get; set; } 
         public string Description { get; set; }
     }
 }
--- RAShop.Shared/DTO/ProductDTO.cs
+++ RAShop.Shared/DTO/ProductDTO.cs
@@ -7,7 +7,6 @@ namespace RAShop.Shared.DTO
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal Price { get; set; }
-        public int Quantity { get; set; }
         public string Description { get; set; }
         public string Origin { get; set; }
         public string Unit { get; set; }
