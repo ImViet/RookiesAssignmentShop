2103d632b11ea02fc5236ebe3817ad8cfb0eaf60
ImViet <dinhquocviet318@gmail.com>
2022-10-20 22:49:09 +0700

    Update code

--- RAShop.Shared/DTO/CategoryDTO.cs
+++ RAShop.Shared/DTO/CategoryDTO.cs
@@ -5,6 +5,6 @@
         public int CategoryId { get; set; }
         public string CategoryName { get; set; } 
         public string Description { get; set; }
-        public IList<SubCateDTO>? SubCates{get; set;}
+        public IList<SubCateDTO>? SubCates{get; set;} = null;
     }
 }
