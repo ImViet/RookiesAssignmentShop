563b7f68dcf5429d46c91fa34ee1752efda7e177
ImViet <dinhquocviet318@gmail.com>
2022-10-27 23:27:50 +0700

    Update code

--- RAShop.Shared/DTO/PagingDTO.cs
+++ RAShop.Shared/DTO/PagingDTO.cs
@@ -3,7 +3,7 @@ namespace RAShop.Shared.DTO
     public class PagingDTO
     {
         public int TotalPages {get; set;}
-        public const int PAGESIZE = 4;
+        public const int PAGESIZE = 8;
         public List<ProductDTO>? Products {get; set;}
     }
 }
